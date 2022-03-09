c8a66e9cf50a6b4f4dd1d9932644fec3414baf93
Mohamed El Baha <mohamed.elbaha@outlook.fr>
2022-03-09 16:26:17 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-331dc264ef832b00dcd9.js"
+    "url": "webpack-runtime-a7a98b07f6e16fd3072d.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ca608897760c7e739a5933cf8ad60a04"
+    "revision": "098042efbe2f45cdfb074e47e36f5020"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
